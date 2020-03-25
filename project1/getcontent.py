a9a6111973ed0eab1aae7647363a79c757c6cba5
nguyenlinh-uet <kenny.nguyen.mat@gmail.com>
2020-03-25 10:55:56 +0700

    tự động lấy nội dung và tách câu

--- project1/getcontent.py
+++ project1/getcontent.py
@@ -1,5 +1,19 @@
 import framler
+import os
+from underthesea import sent_tokenize
+
 dt = framler.NewspapersParser("vnexpress")
-url = "https://vnexpress.net/thoi-su/ha-tinh-to-chuc-cach-ly-tu-cap-xa-4074307.html"
-article = dt.parse(url)
-print(article.text)
+with open("links.txt", "r") as f:
+    urls = f.readlines()
+for url in urls:
+    article = dt.parse(url)
+    # url.split("/")[3] trả về loại bài báo
+    directory_save = "data/" + url.split("/")[3]
+    # nếu folder chưa tồn tại thì tạo mới
+    if not os.path.exists(directory_save):
+        os.makedirs(directory_save,exist_ok=True)
+    sentences = sent_tokenize(article.text)
+    f = open(directory_save + "/" +"data.txt","w",encoding='utf-8')
+    for sentence in sentences:
+        f.write(sentence+ "\n")
+    f.close()
